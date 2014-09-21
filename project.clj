86c1f764e7144199e2fd0fb5e9e539ea9d5611dd
OHTA Shogo <athos0220@gmail.com>
2014-09-21 23:32:59 +0900

    first commit

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject reacta "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/core.async "0.1.338.0-5c5012-alpha"]
+                 [com.stuartsierra/component "0.2.2"]]
+  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.6"]
+                                  [com.cemerick/pomegranate "0.3.0"]]
+                   :source-paths ["dev"]}}
+  :global-vars {*warn-on-reflection* true})
---
+++ test/reacta/core_test.clj
@@ -0,0 +1,7 @@
+(ns reacta.core-test
+  (:require [clojure.test :refer :all]
+            [reacta.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
