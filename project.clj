50f2490933fc061a4c6437e138a5c0f0ea009fbb
OHTA Shogo <athos0220@gmail.com>
2014-09-21 23:54:17 +0900

    Add URL to project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject reacta "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/athos/reacta"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
