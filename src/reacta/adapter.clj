3498c783b647fd8448564de6028b888fadad22f4
OHTA Shogo <athos0220@gmail.com>
2014-09-23 16:43:31 +0900

    Add another namespace for adapter interface

---
+++ src/reacta/adapter.clj
@@ -0,0 +1,16 @@
+(ns reacta.adapter
+  (:refer-clojure :exclude [send])
+  (:require [reacta.platform :as p]))
+
+(defprotocol Adapter
+  (send [this msg])
+  (reply [this msg])
+  (emote [this msg])
+  (topic [this msg])
+  (play [this msg]))
+
+(defn emit [event & more]
+  (apply p/emit event more))
+
+(defn receive [msg]
+  (p/emit :message :content msg))
