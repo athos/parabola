2d27190bdbd06ae4bfed166299461ad1b8880f59
OHTA Shogo <athos0220@gmail.com>
2014-09-27 16:04:23 +0900

    Make adapter dynamically loadable from classpath

--- src/reacta/core.clj
+++ src/reacta/core.clj
@@ -2,16 +2,27 @@
   (:require [clojure.core.async :as a]
             [reacta.platform :as p]
             [com.stuartsierra.component :as comp]
+            [bultitude.core :refer [namespaces-on-classpath]]
             [reacta.adapter :as adapter]
-            [reacta.adapters.shell :as shell]
             reacta.scripts.hello))
+
+(def ^:const ADAPTER_PREFIX "reacta.adapters")
+
+(defn symbol-concat [& syms]
+  (symbol (apply str syms)))
+
+(defn load-adapter [name]
+  (let [ns-name (symbol-concat ADAPTER_PREFIX '. name)]
+    (require ns-name)
+    (when-let [v (find-var (symbol-concat ns-name '/ name))]
+      (@v))))
 
 (extend-protocol comp/Lifecycle
   reacta.adapter.Lifecycle
   (start [this] (adapter/start this))
   (stop [this] (adapter/stop this)))
 
-(def adapter (shell/shell))
+(def adapter (load-adapter "shell"))
 
 (defn run []
   (a/go-loop []
