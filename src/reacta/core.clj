133dd074cd0a16089088ff448da8cefc0279a8f8
OHTA Shogo <athos0220@gmail.com>
2014-09-27 16:22:25 +0900

    Make scripts dynamically loadable from classpath

--- src/reacta/core.clj
+++ src/reacta/core.clj
@@ -3,10 +3,10 @@
             [reacta.platform :as p]
             [com.stuartsierra.component :as comp]
             [bultitude.core :refer [namespaces-on-classpath]]
-            [reacta.adapter :as adapter]
-            reacta.scripts.hello))
+            [reacta.adapter :as adapter]))
 
 (def ^:const ADAPTER_PREFIX "reacta.adapters")
+(def ^:const SCRIPT_PREFIX "reacta.scripts")
 
 (defn symbol-concat [& syms]
   (symbol (apply str syms)))
@@ -17,12 +17,18 @@
     (when-let [v (find-var (symbol-concat ns-name '/ name))]
       (@v))))
 
+(defn load-scripts []
+  (doseq [ns-name (namespaces-on-classpath :prefix SCRIPT_PREFIX)]
+    (require ns-name)))
+
 (extend-protocol comp/Lifecycle
   reacta.adapter.Lifecycle
   (start [this] (adapter/start this))
   (stop [this] (adapter/stop this)))
 
 (def adapter (load-adapter "shell"))
+
+(load-scripts)
 
 (defn run []
   (a/go-loop []
