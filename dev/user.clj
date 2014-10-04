61930d332fda44db4fcf16356d1f57a205ffd0f2
OHTA Shogo <athos0220@gmail.com>
2014-10-04 23:20:33 +0900

    Update dev/user.clj to complete Stuart's workflow

--- dev/user.clj
+++ dev/user.clj
@@ -3,4 +3,24 @@
             [clojure.repl :refer :all]
             [clojure.pprint :refer [pp pprint]]
             [reacta.core :refer :all]
-            [reacta.platform :refer :all]))
+            [reacta.platform :refer :all]
+            [com.stuartsierra.component :as comp]))
+
+(def system nil)
+
+(defn init []
+  (alter-var-root #'system (constantly (reacta-system))))
+
+(defn start []
+  (alter-var-root #'system comp/start))
+
+(defn stop []
+  (alter-var-root #'system comp/stop))
+
+(defn go []
+  (init)
+  (start))
+
+(defn reset []
+  (stop)
+  (refresh :after 'user/go))
