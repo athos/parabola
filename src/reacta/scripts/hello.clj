287e706102556c4852ebdf13821a39a800fda931
OHTA Shogo <athos0220@gmail.com>
2014-10-17 00:01:42 +0900

    Add another simple script

--- src/reacta/scripts/hello.clj
+++ src/reacta/scripts/hello.clj
@@ -5,6 +5,9 @@
 (defreactor :connected [robot message]
   (println "connected!"))
 
+(defreactor #"@reacta_test ping" [robot]
+  (react robot {:type :message :content "pong"}))
+
 (defreactor #"hello" [robot]
   (react robot {:type :message :content "hello"}))
 
