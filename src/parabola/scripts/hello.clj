5c5673687dd7f3ace5915a075118344e311ff85d
OHTA Shogo <athos0220@gmail.com>
2015-03-23 00:25:00 +0900

    Add middleware example

--- src/parabola/scripts/hello.clj
+++ src/parabola/scripts/hello.clj
@@ -1,5 +1,5 @@
 (ns parabola.scripts.hello
-  (:require [parabola.script :refer [defreactors message]]
+  (:require [parabola.script :refer [defreactors reactors message]]
             [clojure.java.shell :refer [sh]]
             [taoensso.timbre :as timbre]))
 
@@ -14,6 +14,17 @@
     (clojure.string/trim-newline (:out (sh "date"))))
   (message #"hello" {{:keys [name]} :user}
     (str "@" name " hello")))
+
+(defn wrap-forwarding-to-twitter [reactors]
+  (fn [msg]
+    (let [res (reactors msg)]
+      (assoc res :adapter :twitter))))
+
+(def ^:reactor forwarding-reactors
+  (-> (reactors
+        (message #"twitter (.+)$" {[_ content] :matches}
+          content))
+      wrap-forwarding-to-twitter))
 
 (defn ^:reactor log-closed [msg]
   (when (= (:type msg) :close)
