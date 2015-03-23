2929d78d41825203d27c5478ca19f686bd980a22
OHTA Shogo <athos0220@gmail.com>
2015-03-23 21:32:31 +0900

    Fix bug

--- src/parabola/middleware.clj
+++ src/parabola/middleware.clj
@@ -2,7 +2,7 @@
 
 (defn forwarding-to [reactors adapter]
   (fn [msg]
-    (let [res (reactors msg)]
+    (when-let [res (reactors msg)]
       (assoc res :adapter adapter))))
 
 (defn only-when [reactors pred]
